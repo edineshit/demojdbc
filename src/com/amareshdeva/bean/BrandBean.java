e037e84affadb637c2f3990d443a480782b61e69
suresh <suresh@suresh-PC>
2017-03-22 08:39:19 +0530

    add db and xml by dinesh

---
+++ src/com/amareshdeva/bean/BrandBean.java
@@ -0,0 +1,39 @@
+package com.amareshdeva.bean;
+
+public class BrandBean {
+
+	private int id;
+	private String brandname;
+	private String discription;
+	private String status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBrandname() {
+		return brandname;
+	}
+	public void setBrandname(String brandname) {
+		this.brandname = brandname;
+	}
+	public String getDiscription() {
+		return discription;
+	}
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	
+	
+	
+}
